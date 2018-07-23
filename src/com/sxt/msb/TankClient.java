cbfcfac10d02e4062cbe97886dce15f52bde44a9
hackaming <2251275876@qq.com>
2018-07-23 15:11:18 +0800

    Signed-off-by: hackaming <2251275876@qq.com> Version 0.2

--- src/com/sxt/msb/TankClient.java
+++ src/com/sxt/msb/TankClient.java
@@ -1,13 +1,26 @@
 package com.sxt.msb;
+
 import java.awt.*;
-public class TankClient extends Frame{
-	public static void main(String[] argv){
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankClient extends Frame {
+	public static void main(String[] argv) {
 		TankClient tc = new TankClient();
 		tc.launchFrame();
 	}
-	public void launchFrame(){
+
+	public void launchFrame() {
 		this.setLocation(400, 300);
 		this.setSize(800, 600);
+		this.setTitle("TankWar");
+		this.setResizable(false);
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 		this.setVisible(true);
 	}
 }
