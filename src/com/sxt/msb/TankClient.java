7e4846061173d07443e6ad7eaaf07931254cb8aa
Yan <Xianming.Yan@us.intercall.com>
2018-07-24 11:00:31 +0800

    Signed-off-by: Yan <Xianming.Yan@us.intercall.com> version:0.5

--- src/com/sxt/msb/TankClient.java
+++ src/com/sxt/msb/TankClient.java
@@ -5,14 +5,33 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankClient extends Frame {
+	public static final int GAME_WIDTH = 800;
+	public static final int GAME_HEIGHT = 600;
+	public static final int GAME_STARTX = 400;
+	public static final int GAME_STARTY = 100;
+	Image offScreenImage = null;
+	
+	@Override
+	public void update(Graphics g) {
+		if (offScreenImage == null){
+			offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+		}
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		Color c = gOffScreen.getColor();
+		gOffScreen.setColor(Color.GREEN);
+		gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+		gOffScreen.setColor(c);
+		paint(gOffScreen);
+		g.drawImage(offScreenImage, 0, 0, null);
+	}
 	int x=50,y=50;
 	public static void main(String[] argv) {
 		TankClient tc = new TankClient();
 		tc.launchFrame();
 	}
 	public void launchFrame() {
-		this.setLocation(400, 300);
-		this.setSize(800, 600);
+		this.setLocation(GAME_STARTX, GAME_STARTY);
+		this.setSize(GAME_WIDTH, GAME_HEIGHT);
 		this.setTitle("TankWar");
 		this.setResizable(false);
 		this.addWindowListener(new WindowAdapter() {
